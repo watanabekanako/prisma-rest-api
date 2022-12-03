46612a364986904a81b6a63ecb55a872f70dd1ee
“Kanako_Watanabe” <109854906+watanabekanako@users.noreply.github.com>
2022-12-03 15:45:30 +0900

    prisma　クエリ途中

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,25 +1,70 @@
+// import { PrismaClient } from "@prisma/client";
+// const prisma = new PrismaClient();
+
+// async function main() {
+//   const taro = await prisma.user.upsert({
+//     where: { email: "taro@example.com" },
+//     update: {},
+//     create: {
+//       email: "taro@example.com",
+//       name: "taro",
+//     },
+//   });
+
+//   const jiro = await prisma.user.upsert({
+//     where: { email: "jiro@example.com" },
+//     update: {},
+//     create: {
+//       email: "jiro@example.com",
+//       name: "jiro",
+//     },
+//   });
+//   console.log({ taro, jiro });
+// }
+
+// main()
+//   .catch((e) => {
+//     console.error(e);
+//     process.exit(1);
+//   })
+//   .finally(async () => {
+//     await prisma.$disconnect();
+//   });
+
 import { PrismaClient } from "@prisma/client";
+
 const prisma = new PrismaClient();
 
+const posts: any = [];
+
+for (let i = 0; i < 100; i++) {
+  posts.push({ title: `title ${i + 1}` });
+}
+
 async function main() {
-  const taro = await prisma.user.upsert({
-    where: { email: "taro@example.com" },
-    update: {},
-    create: {
-      email: "taro@example.com",
-      name: "taro",
+  const alice = await prisma.user.create({
+    data: {
+      name: "Alice",
+      email: "alice@example.com",
+      posts: {
+        create: posts,
+      },
     },
   });
 
-  const jiro = await prisma.user.upsert({
-    where: { email: "jiro@example.com" },
-    update: {},
-    create: {
-      email: "jiro@example.com",
-      name: "jiro",
+  const bob = await prisma.user.create({
+    data: {
+      name: "Bob",
+      email: "bob@example.com",
+      posts: {
+        create: {
+          title: "Check out Prisma with Next.js",
+        },
+      },
     },
   });
-  console.log({ taro, jiro });
+
+  console.log({ alice, bob });
 }
 
 main()
