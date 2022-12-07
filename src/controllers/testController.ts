94178388efe4ba26eee80b0e6e140b5918d58d3f
“Kanako_Watanabe” <109854906+watanabekanako@users.noreply.github.com>
2022-12-07 19:01:43 +0900

    post エンドポイント確認中

--- src/controllers/testController.ts
+++ src/controllers/testController.ts
@@ -6,12 +6,18 @@ const router = Router();
 
 // POST /test
 router.post("/", async (req: Request, res: Response) => {
-  const test = await prisma.test.findMany();
+  const test = await prisma.test.create;
   res.json({ test });
 });
 
 // GET /test
 router.get("/", async (req: Request, res: Response) => {
+  const test = await prisma.test.findMany();
+  res.json({ test });
+});
+
+// DELETE /test
+router.delete("/", async (req: Request, res: Response) => {
   const test = await prisma.test.findMany();
   res.json({ test });
 });
